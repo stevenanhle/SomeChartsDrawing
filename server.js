9d60688c2ad10371899ce3b88cba24e799f94861
Son Le <steven87.le@gmail.com>
2020-08-28 08:58:01 -0400

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<somechartsdrawing>'));
+app.use(express.static(__dirname + '/dist/somechartsdrawing'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<somechartsdrawing>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/somechartsdrawing/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
